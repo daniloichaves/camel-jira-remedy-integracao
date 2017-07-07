7d7f6eb311cad6ec53712ddbe72747c966c65200
Danilo Ischiavolini Chaves <dischiavolini@indracompany.com>
2017-07-07 16:17:49 -0300

    Ajustes alteração para subida projeto

--- src/main/java/br/com/produbanbr/Application.java
+++ src/main/java/br/com/produbanbr/Application.java
@@ -17,65 +17,53 @@
 package br.com.produbanbr;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan
+// @EnableJpaRepositories(basePackages = "br.com.produbanbr")
 public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-    @Component
-    class RestApi extends RouteBuilder {
+	@Component
+	class RestApi extends RouteBuilder {
 
-        @Override
-        public void configure() {
-            restConfiguration()
-                .contextPath("/camel-jira-remey").apiContextPath("/api-doc")
-                    .apiProperty("api.title", "Camel Jira Remedy Integration")
-                    .apiProperty("api.version", "1.0")
-                    .apiProperty("cors", "true")
-                    .apiContextRouteId("doc-api")
-                .bindingMode(RestBindingMode.json);
+		@Override
+		public void configure() {
+			restConfiguration().contextPath("/camel-jira-remedy").apiContextPath("/api-doc")
+					.apiProperty("api.title", "Camel Jira Remedy Integration").apiProperty("api.version", "1.0")
+					.apiProperty("cors", "true").apiContextRouteId("doc-api").bindingMode(RestBindingMode.json);
 
-            rest("/books").description("Books REST service")
-                .get("/").description("The list of all the books")
-                    .route().routeId("books-api")
-                    .bean(Database.class, "findBooks")
-                    .endRest()
-                .get("order/{id}").description("Details of an order by id")
-                    .route().routeId("order-api")
-                    .bean(Database.class, "findOrder(${header.id})");
-        }
-    }
+			rest("/books").description("Books REST service").get("/").description("The list of all the books").route()
+					.routeId("books-api").bean(Database.class, "findBooks").endRest().get("order/{id}")
+					.description("Details of an order by id").route().routeId("order-api")
+					.bean(Database.class, "findOrder(${header.id})");
+		}
+	}
 
-    @Component
-    class Backend extends RouteBuilder {
+	@Component
+	class Backend extends RouteBuilder {
 
-        @Override
-        public void configure() {
-            // A first route generates some orders and queue them in DB
-            from("timer:new-order?delay=1s&period={{example.generateOrderPeriod:2s}}")
-                .routeId("generate-order")
-                .bean("orderService", "generateOrder")
-                .to("jpa:org.apache.camel.example.spring.boot.rest.jpa.Order")
-                .log("Inserted new order ${body.id}");
+		@Override
+		public void configure() {
+			// A first route generates some orders and queue them in DB
+			from("timer:new-order?delay=1s&period={{example.generateOrderPeriod:2s}}").routeId("generate-order")
+					.bean("orderService", "generateOrder").to("jpa:org.apache.camel.example.spring.boot.rest.jpa.Order")
+					.log("Inserted new order ${body.id}");
 
-            // A second route polls the DB for new orders and processes them
-            from("jpa:org.apache.camel.example.spring.boot.rest.jpa.Order"
-                + "?consumer.namedQuery=new-orders"
-                + "&consumer.delay={{example.processOrderPeriod:5s}}"
-                + "&consumeDelete=false")
-                .routeId("process-order")
-                .log("Processed order #id ${body.id} with ${body.amount} copies of the «${body.book.description}» book");
-        }
-    }
+			// A second route polls the DB for new orders and processes them
+			from("jpa:org.apache.camel.example.spring.boot.rest.jpa.Order" + "?consumer.namedQuery=new-orders"
+					+ "&consumer.delay={{example.processOrderPeriod:5s}}"
+					+ "&consumeDelete=false").routeId("process-order").log(
+							"Processed order #id ${body.id} with ${body.amount} copies of the «${body.book.description}» book");
+		}
+	}
 }
--- src/main/java/br/com/produbanbr/BookRepository.java
+++ src/main/java/br/com/produbanbr/BookRepository.java
@@ -17,6 +17,8 @@
 package br.com.produbanbr;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
 }
